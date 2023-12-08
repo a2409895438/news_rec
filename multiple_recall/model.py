2a62016a3724afe36e34fb28a92efbe521fb432a
yangdongliu <yangdongliu@tencent.com>
2023-12-08 15:18:07 +0800

    icf

--- multiple_recall/model.py
+++ multiple_recall/model.py
@@ -3,14 +3,14 @@ from tqdm import tqdm
 import pickle
 import numpy as np
 import pandas as pd
-from collections import defaultdict
-from ..utils import *
+from collections import defaultdict,Counter
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from deepctr.feature_column import SparseFeat, VarLenSparseFeat
 from deepmatch.models import *
-from deepmatch.utils import sampledsoftmaxloss
+from deepmatch.utils import sampledsoftmaxloss,NegativeSampler
 from tensorflow.python.keras.models import Model
+from utils import *
 
 
 def itemcf_sim(df, item_created_time_dict,save_path):
@@ -147,8 +147,11 @@ def youtubednn_u2i_dict(data,save_path, topk=20):
     item_feature_columns = [SparseFeat('click_article_id', feature_max_idx['click_article_id'], embedding_dim)]
     
     # 模型的定义 
-    # num_sampled: 负采样时的样本数量
-    model = YoutubeDNN(user_feature_columns, item_feature_columns, num_sampled=5, user_dnn_hidden_units=(64, embedding_dim))
+    # num_sampled: 负采样时的样本数量  版本更新，修改为sampler_config
+    train_counter = Counter(train_model_input['click_article_id'])
+    item_count = [train_counter.get(i,0) for i in range(item_feature_columns[0].vocabulary_size)]
+    sampler_config = NegativeSampler('frequency',num_sampled=5,item_name="click_article_id",item_count=item_count)
+    model = YoutubeDNN(user_feature_columns, item_feature_columns, user_dnn_hidden_units=(64, embedding_dim),sampler_config=sampler_config)
     # 模型编译
     model.compile(optimizer="adam", loss=sampledsoftmaxloss)  
     
@@ -204,4 +207,116 @@ def youtubednn_u2i_dict(data,save_path, topk=20):
     # 可以直接对这个召回结果进行评估，为了方便可以统一写一个评估函数对所有的召回结果进行评估
     pickle.dump(user_recall_items_dict, open(save_path + 'youtube_u2i_dict.pkl', 'wb'))
     return user_recall_items_dict
+
+# 基于商品的召回i2i
+def item_based_recommend(user_id, user_item_time_dict, i2i_sim, sim_item_topk, recall_item_num, item_topk_click, item_created_time_dict, emb_i2i_sim):
+    """
+        基于文章协同过滤的召回
+        :param user_id: 用户id
+        :param user_item_time_dict: 字典, 根据点击时间获取用户的点击文章序列   {user1: {item1: time1, item2: time2..}...}
+        :param i2i_sim: 字典，文章相似性矩阵
+        :param sim_item_topk: 整数， 选择与当前文章最相似的前k篇文章
+        :param recall_item_num: 整数， 最后的召回文章数量
+        :param item_topk_click: 列表，点击次数最多的文章列表，用户召回补全
+        :param emb_i2i_sim: 字典基于内容embedding算的文章相似矩阵
+        
+        return: 召回的文章列表 {item1:score1, item2: score2...}
+        
+    """
+    # 获取用户历史交互的文章
+    user_hist_items = user_item_time_dict[user_id]
+    
+    item_rank = {}
+    for loc, (i, click_time) in enumerate(user_hist_items):
+        for j, wij in sorted(i2i_sim[i].items(), key=lambda x: x[1], reverse=True)[:sim_item_topk]:
+            if j in user_hist_items:
+                continue
+            
+            # 文章创建时间差权重
+            created_time_weight = np.exp(0.8 ** np.abs(item_created_time_dict[i] - item_created_time_dict[j]))
+            # 相似文章和历史点击文章序列中历史文章所在的位置权重
+            loc_weight = (0.9 ** (len(user_hist_items) - loc))
+            
+            content_weight = 1.0
+            if emb_i2i_sim.get(i, {}).get(j, None) is not None:
+                content_weight += emb_i2i_sim[i][j]
+            if emb_i2i_sim.get(j, {}).get(i, None) is not None:
+                content_weight += emb_i2i_sim[j][i]
+                
+            item_rank.setdefault(j, 0)
+            item_rank[j] += created_time_weight * loc_weight * content_weight * wij
+    
+    # 不足10个，用热门商品补全
+    if len(item_rank) < recall_item_num:
+        for i, item in enumerate(item_topk_click):
+            if item in item_rank.items(): # 填充的item应该不在原来的列表中
+                continue
+            item_rank[item] = - i - 100 # 随便给个负数就行
+            if len(item_rank) == recall_item_num:
+                break
+    
+    item_rank = sorted(item_rank.items(), key=lambda x: x[1], reverse=True)[:recall_item_num]
+        
+    return item_rank
+
+
+
+# 基于用户的召回 u2u2i
+def user_based_recommend(user_id, user_item_time_dict, u2u_sim, sim_user_topk, recall_item_num, 
+                         item_topk_click, item_created_time_dict, emb_i2i_sim):
+    """
+        基于文章协同过滤的召回
+        :param user_id: 用户id
+        :param user_item_time_dict: 字典, 根据点击时间获取用户的点击文章序列   {user1: {item1: time1, item2: time2..}...}
+        :param u2u_sim: 字典，文章相似性矩阵
+        :param sim_user_topk: 整数， 选择与当前用户最相似的前k个用户
+        :param recall_item_num: 整数， 最后的召回文章数量
+        :param item_topk_click: 列表，点击次数最多的文章列表，用户召回补全
+        :param item_created_time_dict: 文章创建时间列表
+        :param emb_i2i_sim: 字典基于内容embedding算的文章相似矩阵
+        
+        return: 召回的文章列表 {item1:score1, item2: score2...}
+    """
+    # 历史交互
+    user_item_time_list = user_item_time_dict[user_id]    # {item1: time1, item2: time2...}
+    user_hist_items = set([i for i, t in user_item_time_list])   # 存在一个用户与某篇文章的多次交互， 这里得去重
+    
+    items_rank = {}
+    for sim_u, wuv in sorted(u2u_sim[user_id].items(), key=lambda x: x[1], reverse=True)[:sim_user_topk]:
+        for i, click_time in user_item_time_dict[sim_u]:
+            if i in user_hist_items:
+                continue
+            items_rank.setdefault(i, 0)
+            
+            loc_weight = 1.0
+            content_weight = 1.0
+            created_time_weight = 1.0
+            
+            # 当前文章与该用户看的历史文章进行一个权重交互
+            for loc, (j, click_time) in enumerate(user_item_time_list):
+                # 点击时的相对位置权重
+                loc_weight += 0.9 ** (len(user_item_time_list) - loc)
+                # 内容相似性权重
+                if emb_i2i_sim.get(i, {}).get(j, None) is not None:
+                    content_weight += emb_i2i_sim[i][j]
+                if emb_i2i_sim.get(j, {}).get(i, None) is not None:
+                    content_weight += emb_i2i_sim[j][i]
+                
+                # 创建时间差权重
+                created_time_weight += np.exp(0.8 * np.abs(item_created_time_dict[i] - item_created_time_dict[j]))
+                
+            items_rank[i] += loc_weight * content_weight * created_time_weight * wuv
+        
+    # 热度补全
+    if len(items_rank) < recall_item_num:
+        for i, item in enumerate(item_topk_click):
+            if item in items_rank.items(): # 填充的item应该不在原来的列表中
+                continue
+            items_rank[item] = - i - 100 # 随便给个复数就行
+            if len(items_rank) == recall_item_num:
+                break
+        
+    items_rank = sorted(items_rank.items(), key=lambda x: x[1], reverse=True)[:recall_item_num]    
+    
+    return items_rank
 
